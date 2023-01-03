e6351cacf3d6d796f27f78c2fed78cfb1ec06dda
Albert Lesniewski <mikolaj.albert.lesniewski@gmail.com>
2023-01-03 23:01:17 +0100

    initial commit

---
+++ src/QuarkApp.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function QuarkApp() {
+    return (
+        <h1>Hello, Quark!</h1>
+    )
+};
+
+export default QuarkApp;
---
+++ src/index.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import QuarkApp from './QuarkApp';
+
+ReactDOM.render(
+    <QuarkApp/>,
+    document.getElementById('app')
+);
